e9b538c89804a4d59aaa74b320bb627935302a54
Your Name <you@example.com>
2016-10-22 16:26:19 +0900

    test3

--- src/main/java/com/ningen/gomi/drone/java/Main.java
+++ src/main/java/com/ningen/gomi/drone/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello, world!");
         System.out.println("hello, world!");
-        System.out.println("hello, world!");
     }
 
     public static int getOne() {
